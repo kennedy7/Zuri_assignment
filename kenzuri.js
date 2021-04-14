63b3fd8eaad025d5e5d05ecd8783bdf2942f0153
kennedy7 <kennedyhillary6@gmail.com>
2021-04-14 16:53:23 +0100

    initial commit

---
+++ kenzuri.js
@@ -0,0 +1,8 @@
+const  fs = require('fs');
+const http= require ('http');
+const url= require ('url');
+const fetch = require ('node-fetch')
+
+fetch('http://jsonplaceholder.typicode.com/posts' ).then(res=>res.text())
+.then(text=>fs.writeFile('./result/json.txt',text, 'utf-8', err=>{  
+     console.log('your files have been fetched and written')}))
